4eea532a4101b99822194b817206339a0f7884ca
FernandoChurros <kappapridelgbt@gmail.com>
2021-04-16 14:54:53 -0300

    add section type of datas, and complete articles, Strings, Numbers and Boolean

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-const htmlItem = document.querySelectorAll('.items_list');
+const listNav = document.querySelectorAll('.list_nav');
+const itemsList = document.querySelectorAll('.items_list');
 
-function show(id) {
-   const item = document.querySelector(`${id}`);
-   item.classList.toggle('active');
+// function show(id) {
+//    const item = document.querySelector(`${id}`);
+//    item.classList.toggle('active');
+// }
+
+// document.querySelector('#html_list').addEventListener('click', () => {
+//    show('#html');
+// })
+
+// document.querySelector('#css_list').addEventListener('click', () => {
+//    show('#css');
+// })
+
+for (let i = 0; i < listNav.length; i++) {
+   listNav[i].onclick = () => {
+
+      itemsList[i].classList.toggle('active');
+   }
 }
-
-document.querySelector('#html_list').addEventListener('click', () => {
-   show('#html');
-})
-
-document.querySelector('#css_list').addEventListener('click', () => {
-   show('#css');
-})
