e111e9e51557bb7424158e6e54c01963ef3c8209
FernandoChurros <kappapridelgbt@gmail.com>
2021-04-18 17:32:20 -0400

    add page about variable

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
 const listNav = document.querySelectorAll('.list_nav');
 const itemsList = document.querySelectorAll('.items_list');
 
-// function show(id) {
-//    const item = document.querySelector(`${id}`);
-//    item.classList.toggle('active');
-// }
-
-// document.querySelector('#html_list').addEventListener('click', () => {
-//    show('#html');
-// })
-
-// document.querySelector('#css_list').addEventListener('click', () => {
-//    show('#css');
-// })
-
 for (let i = 0; i < listNav.length; i++) {
    listNav[i].onclick = () => {
 
